503e8c75f0d26791588047e8289e66b5af072184
Ashit Sood <sooda@codeaurora.org>
2018-06-03 16:37:38 -0700

    IMS: Add check for RTT call
    - Add check to send RTT message for
      call connected as RTT call
    
    - Add check to allow upgrding the call
      if it is an Emergency call
    
    - Add check to accept incoming call as RTT
      call even if RTT is off on UI
    
    Change-Id: I77334e1948ce0d95fc50db03970cad58f4065c35
    CRs-Fixed: 2254779
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -4272,7 +4272,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
 
     // Update the Rtt attribute
     private ImsCallProfile setRttModeBasedOnOperator(ImsCallProfile profile) {
-        if (!mPhone.canProcessRttReqest()) {
+        if (!(mPhone.isRttSupported() && mPhone.isRttOn())) {
             return profile;
         }
 
@@ -4293,7 +4293,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
     private ImsStreamMediaProfile addRttAttributeIfRequired(ImsCall call,
             ImsStreamMediaProfile mediaProfile) {
 
-        if (!mPhone.canProcessRttReqest()) {
+        if (!mPhone.isRttSupported()) {
             return mediaProfile;
         }
 
