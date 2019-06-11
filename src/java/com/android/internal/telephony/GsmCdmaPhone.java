5037237e1bf8b6d4eb5ab40b16df6fc76d4a1ace
Pooja Jain <poojain@codeaurora.org>
2019-06-11 11:31:37 +0530

    IMS: Fix NullPointerException if countryIso is null
    Fix NPE which occured due to countryIso field being null in the subscription
    info.
    
    Change-Id: I203282e49e90e14f49b111dea144dd5cafefb2fc
    CRs-Fixed: 2462327
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -3792,7 +3792,11 @@ public class GsmCdmaPhone extends Phone {
         if (subInfo == null) {
             return null;
         }
-        return subInfo.getCountryIso().toUpperCase();
+        final String country = subInfo.getCountryIso();
+        if (country == null) {
+            return null;
+        }
+        return country.toUpperCase();
     }
 
     public void notifyEcbmTimerReset(Boolean flag) {
